2954c104446ec2b542bdbbc8380780875bb9d5e7
Ludovico Bianchi <me@ludob.com>
2019-02-22 19:36:54 -0800

    add automatic way to download input data

--- tasks.py
+++ tasks.py
@@ -1,19 +1,21 @@
 from invoke import task
+from pathlib import Path
 from zipfile import ZipFile
 from tqdm import tqdm
+
 
 import config as c
 
 
 @task
-def package_input_data(ctx, path_archive=c.PATH_ARCHIVES / 'input-data.zip'):
+def package_input_data(ctx, path_archive=c._PATH_ARCHIVES / 'input-data.zip'):
     path_to_archive = c.PATH_HTML_URLS
     with ZipFile(path_archive, 'w') as z:
         z.write(path_to_archive, path_to_archive.name)
         print(z)
 
 @task
-def package_intermediate_results(ctx, path_archive=c.PATH_ARCHIVES / 'intermediate-results.zip'):
+def package_intermediate_results(ctx, path_archive=c._PATH_ARCHIVES / 'intermediate-results.zip'):
     path_to_archive = c.PATH_INTERMEDIATE_RESULTS
     to_archive = path_to_archive.rglob('*')
 
@@ -22,3 +24,12 @@ def package_intermediate_results(ctx, path_archive=c.PATH_ARCHIVES / 'intermedia
             path_in_archive = path_source.relative_to(path_to_archive)
             z.write(path_source, path_in_archive)
         print(z)
+
+
+@task
+def install_input_data(ctx, url=c._URL_INPUT_DATA_ARCHIVE, path=c.PATH_INPUT):
+    ctx.run(f'wget {url}', pty=True)
+    path_archive = Path(Path(url).name)
+    ZipFile(path_archive).extractall(path)
+    ctx.run(f'ls -l {path}')
+    path_archive.unlink()
