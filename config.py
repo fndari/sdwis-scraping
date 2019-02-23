771a3f8f43649b5035c89648245cb6f675742c59
Ludovico Bianchi <me@ludob.com>
2019-02-22 18:20:04 -0800

    update notebooks to new structure

--- config.py
+++ config.py
@@ -1,21 +1,32 @@
 from pathlib import Path
 
+PATH_DATA = Path('./data')
 
-PATH_INPUT = Path('./data-in')
-PATH_DATA = Path('./data-out')
+PATH_INPUT = PATH_DATA / 'input'
+PATH_INTERMEDIATE_RESULTS = PATH_DATA / 'intermediate-results'
+PATH_RESULTS = PATH_DATA / 'results'
 
 PATH_HTML_URLS = PATH_INPUT / 'wsd-urls-js-table.html'
 
-PATH_URLS = PATH_DATA / 'wsd-urls.csv'
+PATH_URLS = PATH_INTERMEDIATE_RESULTS / 'wsd-urls.csv'
+PATH_HTML_PAGES = PATH_INTERMEDIATE_RESULTS / 'html-pages'
+PATH_PARSED_RECORDS = PATH_INTERMEDIATE_RESULTS / 'parsed-records.json'
 
-# change this number to set the number of URLs to process (-1 to process all of them)
-# Running the whole dataset can be somewhat slow (~10 minutes) and memory-intensive (~3 GB)
-# since the processing, especially the parsing, is not really optimized for performance
-N_URLS_TO_PROCESS = 1000
-# without the ".sqlite" suffix
-PATH_REQUESTS_CACHE_DB = PATH_INPUT / 'requests-cache-db'
 PATH_DATAPACKAGE = PATH_DATA / 'datapackage.zip'
 PATH_DATAPACKAGE_METADATA = Path('./datapackage-metadata.yaml')
 
-# N_URLS_TO_PROCESS = -1
-# PATH_REQUESTS_CACHE_DB = Path('/data/datasets/parse-pws')
+PATH_ARCHIVES = PATH_DATA / 'archives'
+
+_PATHS_TO_CREATE = [PATH_INPUT, PATH_INTERMEDIATE_RESULTS, PATH_RESULTS, PATH_HTML_PAGES]
+
+
+def _create_paths(paths):
+    for path in paths:
+        path.mkdir(parents=True, exist_ok=True)
+
+
+# N_URLS_TO_FETCH = 1000
+N_URLS_TO_FETCH = -1
+
+
+_create_paths(_PATHS_TO_CREATE)
